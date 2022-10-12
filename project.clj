4121be403f846bb8816cf9b997c7d3aff9830728
confuzedskull <confuzedskull@mail.com>
2022-10-11 19:36:01 -0700

    replaced cljsbuild with new figwheel-main config

--- project.clj
+++ project.clj
@@ -1,9 +1,4 @@
 (defproject fijit "1.0.2"
-  :cljsbuild {:builds
-              [{:compiler {:optimizations :advanced
-                           :output-to "bin/main.js"
-                           :warnings {:munged-namespace false}}
-                :source-paths ["src"]}]}
   :dependencies [[com.bhauman/figwheel-main "0.2.17"]
                  [com.bhauman/rebel-readline-cljs "0.1.4"]
                  [org.clojure/clojurescript "1.11.4"]
@@ -13,7 +8,6 @@
                  [cljsjs/react "17.0.2-0"]
                  [cljs-ajax "0.8.4"]
                  [reagent "1.1.1"]]
-  :plugins [[lein-cljsbuild "1.1.8"]]
   :resource-paths ["target"]
-  :aliases {"debug" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
-            "release" ["cljsbuild" "once"]})
+  :aliases {"debug" ["trampoline" "run" "-m" "figwheel.main" "-b" "debug" "-r"]
+            "release" ["run" "-m" "figwheel.main" "-bo" "release"]})
